c50c42ae6ff96f6573895cc1a5bb4f2bc9cf8216
Ruaridh Macdonald <rmacd@mit.edu>
2023-10-25 09:56:08 -0400

    Initial commit showing demand and resource structs with generic functions to load resources

---
+++ prototypes/structs_demo_1/src/core/demand.jl
@@ -0,0 +1,35 @@
+using CSV
+using DataFrames
+
+struct Demand
+    zone::String
+    demand::Vector{Float64}
+    sector::String
+    profiletype::String
+end
+
+function makeelecdemand(zone::String, demand::Vector{Float64}, sector::String="electricity", profiletype::String="components")
+    return Demand(zone, demand, sector,  profiletype)
+end
+
+function loadelecdemands(filepath)
+    elec_demands = Demand[]
+    df = CSV.read(filepath, DataFrame)
+    for (name, col) in pairs(eachcol(df))
+        push!(elec_demands, makeelecdemand(string(name), col))
+    end
+    return elec_demands
+end
+
+function calcfft(demand::Vector{Float64}, ffttype::String="real")
+    # TODO: swap all the defaults to real and make the inverse ffts responsive
+    if !(ffttype in ["real", "complex"])
+        error("ffttype must be either real or complex")
+    end
+    if ffttype == "real"
+        fdemand = rfft(demand)
+    elseif ffttype == "complex"
+        fdemand = fft(demand)
+    end
+    return fdemand
+end
---
+++ prototypes/structs_demo_1/src/core/resource.jl
@@ -0,0 +1,27 @@
+using CSV
+using DataFrames
+
+struct Resource
+    name::String
+    type::String
+    zone::String
+    fixedcost::Float64
+    variablecost::Float64
+end
+
+function makeresource(name::String, type::String, zone::String, fixedcost::Float64, variablecost::Float64)
+    return Resource(name, type, zone, fixedcost, variablecost)
+end
+
+function makeresource(row::DataFrameRow)
+    return Resource(row.Name, row.Type, row.Zone, row.Fixed_Cost, row.Variable_Cost)
+end
+
+function loadresources(filepath)
+    resources = Resource[]
+    df = CSV.read(filepath, DataFrame)
+    for row in eachrow(df)
+        push!(resources, makeresource(row))
+    end
+    return resources
+end
