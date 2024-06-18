caa736d042820243bbb0d97ce29a54dd92216d2b
lbonaldo <bonaldo.luca12@gmail.com>
2024-06-18 09:33:46 -0400

    Add transformation attributes to battery

--- src/model/assets/battery.jl
+++ src/model/assets/battery.jl
@@ -9,7 +9,18 @@ function make_battery(data::Dict{Symbol,Any}, time_data::Dict{Symbol,TimeData}, 
     _battery_transform = Transformation(;
         id=:Battery,
         timedata=time_data[:Electricity],
-        stoichiometry_balance_names=get(data, :stoichiometry_balance_names, [:energy])
+        stoichiometry_balance_names=get(data, :stoichiometry_balance_names, [:energy]),
+        can_retire = get(data, :can_retire, false),
+        can_expand = get(data, :can_expand, false),
+        existing_capacity_storage = get(data, :existing_capacity_storage, 0.0),
+        investment_cost_storage = get(data, :investment_cost_storage, 0.0),
+        fixed_om_cost_storage = get(data, :fixed_om_cost_storage, 0.0),
+        storage_loss_fraction = get(data, :storage_loss_fraction, 0.0),
+        min_duration = get(data, :min_duration, 0.0),
+        max_duration = get(data, :max_duration, 0.0),
+        min_storage_level = get(data, :min_storage_level, 0.0),
+        min_capacity_storage = get(data, :min_capacity_storage, 0.0),
+        max_capacity_storage = get(data, :max_capacity_storage, Inf),
     )
     add_constraints!(_battery_transform, data)
 
