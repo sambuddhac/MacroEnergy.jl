c2eb45ad570c03c0ffde8f6f6c8184c24bf4ae86
Filippo Pecci <peccifilippo@gmail.com>
2024-07-17 17:56:21 -0400

    demand and stoichiometry balances are now just balances

--- src/model/constraints/balance.jl
+++ src/model/constraints/balance.jl
@@ -1,18 +1,17 @@
-Base.@kwdef mutable struct StoichiometryBalanceConstraint <:OperationConstraint
+Base.@kwdef mutable struct BalanceConstraint <:OperationConstraint
     value::Union{Missing,Vector{Float64}} = missing
     lagrangian_multiplier::Union{Missing,Vector{Float64}} = missing
     constraint_ref::Union{Missing,JuMPConstraint} = missing
 end
 
 function add_model_constraint!(
-    ct::StoichiometryBalanceConstraint,
-    g::AbstractTransform,
+    ct::BalanceConstraint,
+    v::AbstractVertex,
     model::Model,
 )
 
     ct.constraint_ref =
-        @constraint(model, [i in stoichiometry_balance_names(g), t in time_interval(g)], stoichiometry_balance(g,i,t) == 0.0)
+        @constraint(model, [i in balance_ids(v), t in time_interval(v)], get_balance(v,i,t) == 0.0)
 
     return nothing
 end
-
