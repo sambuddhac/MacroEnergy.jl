cad43b5967d9e678fa305877a301b898dbd7e3b5
Filippo Pecci <peccifilippo@gmail.com>
2024-07-17 18:00:43 -0400

    new node definition with abstract vertex

--- src/model/networks/node.jl
+++ src/model/networks/node.jl
@@ -1,18 +1,11 @@
-Base.@kwdef mutable struct Node{T} <: AbstractNode{T}
-    ### Fields without defaults
-    id::Symbol
-    demand::Union{Vector{Float64},Dict{Int64,Float64}}
-    demand_header::Union{Nothing,Symbol}
-    timedata::TimeData{T}
-    #### Fields with defaults
+Base.@kwdef mutable struct Node{T} <: AbstractVertex
+    @AbstractVertexBaseAttributes()
+    demand::Union{Vector{Float64},Dict{Int64,Float64}} = Vector{Float64}()
+    demand_header::Union{Nothing,Symbol} = nothing
     max_nsd::Vector{Float64} = [0.0]
     price_nsd::Vector{Float64} = [0.0]
-    operation_vars::Dict = Dict()
-    operation_expr::Dict = Dict()
-    planning_vars::Dict = Dict()
     price_unmet_policy::Dict{DataType,Float64} = Dict{DataType,Float64}()
     rhs_policy::Dict{DataType,Float64} = Dict{DataType,Float64}()
-    constraints::Vector{AbstractTypeConstraint} = Vector{AbstractTypeConstraint}()
 end
 
 function make_node(data::Dict{Symbol,Any}, time_data::Dict{Symbol,TimeData}, commodity::DataType)
@@ -33,64 +26,24 @@ end
 Node(data::Dict{Symbol,Any}, time_data::Dict{Symbol,TimeData}, commodity::DataType) = make_node(data, time_data, commodity)
 
 commodity_type(n::AbstractNode{T}) where {T} = T;
-
-get_id(n::AbstractNode) = n.id;
-
 demand(n::AbstractNode) = n.demand;
 demand(n::AbstractNode,t::Int64) = demand(n)[t];
 demand_header(n::AbstractNode) = n.demand_header;
-
 non_served_demand(n::AbstractNode) = n.operation_vars[:non_served_demand];
 non_served_demand(n::AbstractNode,s::Int64,t::Int64) = non_served_demand(n)[s,t];
-
-net_balance(n::AbstractNode) = n.operation_expr[:net_balance];
-net_balance(n::AbstractNode,t::Int64) = net_balance(n)[t];
-
 max_non_served_demand(n::AbstractNode) = n.max_nsd;
 max_non_served_demand(n::AbstractNode,s::Int64) = max_non_served_demand(n)[s];
-
 price_non_served_demand(n::AbstractNode) = n.price_nsd;
 price_non_served_demand(n::AbstractNode,s::Int64) = price_non_served_demand(n)[s];
-
 segments_non_served_demand(n::AbstractNode) = 1:length(n.max_nsd);
-
-all_constraints(n::AbstractNode) = n.constraints;
-
 price_unmet_policy(n::AbstractNode) = n.price_unmet_policy;
 price_unmet_policy(n::AbstractNode,c::DataType) = price_unmet_policy(n)[c];
-
 rhs_policy(n::AbstractNode) = n.rhs_policy;
 rhs_policy(n::AbstractNode,c::DataType) = rhs_policy(n)[c];
 
-function add_operation_variables!(n::AbstractNode, model::Model)
-
-    n.operation_expr[:net_balance] =
-        @expression(model, [t in time_interval(n)], 0 * model[:vREF])
-
-    if !all(max_non_served_demand(n).==0)
-        n.operation_vars[:non_served_demand] = @variable(
-            model,
-            [s in segments_non_served_demand(n) ,t in time_interval(n)],
-            lower_bound = 0.0,
-            base_name = "vNSD_$(get_id(n))"
-        )
-
-        for t in time_interval(n)
-            w = current_subperiod(n,t);
-            for s in segments_non_served_demand(n)
-                add_to_expression!(model[:eVariableCost], subperiod_weight(n,w)*price_non_served_demand(n,s), non_served_demand(n,s,t))
-                add_to_expression!(net_balance(n,t), non_served_demand(n,s,t))
-            end
-        end
-    end
-
-    return nothing
-end
-
-function add_planning_variables!(n::AbstractNode,model::Model)
-
-    if any(isa.(n.constraints,PolicyConstraint))
-        ct_all = findall(isa.(n.constraints,PolicyConstraint));
+function add_planning_variables!(n::Node,model::Model)
+    if in(PolicyConstraint,supertype.(typeof.(n.constraints)))
+        ct_all = findall(PolicyConstraint.==supertype.(typeof.(n.constraints)));
         for ct in ct_all
             
             ct_type = typeof(n.constraints[ct]);
@@ -105,6 +58,39 @@ function add_planning_variables!(n::AbstractNode,model::Model)
     end
     return nothing
 end
+
+function add_operation_variables!(n::Node, model::Model)
+
+    if !isempty(balance_ids(n))
+        for i in balance_ids(n)
+            if i==:demand
+                n.operation_expr[:demand] = @expression(model, [t in time_interval(n)], -demand(n,t) * model[:vREF])
+            else
+                n.operation_expr[i] = @expression(model, [t in time_interval(n)], 0 * model[:vREF])
+            end
+        end
+    end
+
+    if !all(max_non_served_demand(n).==0)
+        n.operation_vars[:non_served_demand] = @variable(
+            model,
+            [s in segments_non_served_demand(n) ,t in time_interval(n)],
+            lower_bound = 0.0,
+            base_name = "vNSD_$(get_id(n))"
+        )
+
+        for t in time_interval(n)
+            w = current_subperiod(n,t);
+            for s in segments_non_served_demand(n)
+                add_to_expression!(model[:eVariableCost], subperiod_weight(n,w)*price_non_served_demand(n,s), non_served_demand(n,s,t))
+                add_to_expression!(get_balance(n,:demand,t), non_served_demand(n,s,t))
+            end
+        end
+    end
+
+    return nothing
+end
+
 
 function get_nodes_sametype(nodes::Dict{Symbol,Node},commodity::DataType)
     return filter(((k,n),) -> commodity_type(n)==commodity, nodes)
