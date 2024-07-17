95828b62c6ed4ff62f11ac989664ae0aa5789fff
RuaridhMacd <rmacd@mit.edu>
2024-07-17 03:46:37 -0400

    Add locations

---
+++ src/model/networks/location.jl
@@ -0,0 +1,23 @@
+Base.@kwdef mutable struct Location <: MacroObject
+    name::String
+    nodes::Dict{Symbol, AbstractNode} = Dict{Symbol, AbstractNode}()
+    commodities::Set{Symbol} = Set{Symbol}()
+    # We could change this to work on commodities, not symbols
+    # My feeling is that symbols will be lighter and faster
+end
+
+function add_node!(loc::Location, node::AbstractNode{T}, replace::Bool=false) where T<:Commodity
+    node_commodity = Symbol(commodity_type(node))
+    # If a node of the same type exists then throw an error unless replace is true
+    if ((node_commodity in loc.commodities) || (node_commodity in keys(loc.nodes))) && !replace
+        error("A $node_commodity node already exists in the $(loc.name) location")
+    end
+    loc.nodes[node_commodity] = node
+    push!(loc.commodities, node_commodity)
+end
+
+function refresh_commodities_list!(loc::Location)
+    loc.commodities = Set{Symbol}(
+        Symbol(commodity_type(node)) for node in values(loc.nodes)
+    )
+end
