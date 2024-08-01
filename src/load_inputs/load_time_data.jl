7677c3cd0904997b27b1b21f725cb1670cfc543b
Filippo Pecci <peccifilippo@gmail.com>
2024-08-01 12:20:04 -0400

    fixed small typo in load_time_data.jl

--- src/load_inputs/load_time_data.jl
+++ src/load_inputs/load_time_data.jl
@@ -46,7 +46,7 @@ function create_time_data(time_data::JSON3.Object, commodities::Dict{Symbol,Data
             time_interval=time_interval,
             subperiods=subperiods,
             subperiod_weights=Dict(eachindex(subperiods) .=> weights_per_subperiod / hours_per_subperiod),
-            subperiod_indices = eachindex(subperiods)
+            subperiod_indices = eachindex(subperiods),
             hours_per_timestep =  time_data[:HoursPerTimeStep][sym]
         )
     end
