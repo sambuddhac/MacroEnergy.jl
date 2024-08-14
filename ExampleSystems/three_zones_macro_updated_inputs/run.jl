6dbfd04b9a3b3bf035e3b2cb51e0349d6a84767d
Ruaridh Macdonald <rmacd@mit.edu>
2024-08-14 12:10:31 -0400

    WIP - finished updating ThreeZones inputs

--- ExampleSystems/three_zones_macro_updated_inputs/run.jl
+++ ExampleSystems/three_zones_macro_updated_inputs/run.jl
@@ -1,40 +1,11 @@
 using Macro
 using Gurobi
 
-test_case_path = joinpath(dirname(dirname(@__DIR__)), "ExampleSystems", "three_zones_macro")
+case_path = @__DIR__
+println("###### ###### ######")
+println("Running case at $(case_path)")
 
-macro_settings = Macro.configure_settings(joinpath(test_case_path, "settings", "macro_settings.yml"))
-
-# Load and create all the Nodes
-system_path = joinpath(test_case_path, "system")
-
-# Read in all the commodities
-commodities = Macro.load_commodities_json(system_path)
-
-# Read in time data
-time_data = Macro.load_time_json(system_path, commodities)
-
-# Read in all the nodes
-network_dir = joinpath(system_path, "network")
-nodes = Macro.load_nodes_json(network_dir, time_data)
-
-# Read in demand data
-Macro.load_demand_data!(nodes, system_path)
-
-# Create the network (aka Edges) between Nodes
-edges = Macro.load_edges_json(network_dir, time_data, nodes)
-
-# Load all the asset data
-asset_dir = joinpath(test_case_path, "assets")
-assets = Macro.load_assets_json(asset_dir, time_data, nodes)
-
-# Load the capacity factor data for the assets
-Macro.load_capacity_factor!(assets, asset_dir)
-
-# Read in fuel data and CO2 emissions
-Macro.load_fuel_data!(system_path, assets)
-
-system = Macro.create_system(nodes, edges, assets)
+system = Macro.load_system(case_path)
 
 model = Macro.generate_model(system)
 
