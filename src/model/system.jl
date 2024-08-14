fda5c72e17a1b7137e659355a6a31b05034d9a29
Ruaridh Macdonald <rmacd@mit.edu>
2024-08-14 09:39:34 -0400

    Add System struct to hold details beyond just Vertex[], Asset[], etc

--- src/model/system.jl
+++ src/model/system.jl
@@ -14,3 +14,28 @@ function create_system(nodes::Dict{Symbol,Node}, edges::Dict{Symbol, Edge}, asse
     end
     return system
 end
+
+Base.@kwdef mutable struct System
+    data_dirpath::String
+    settings::NamedTuple
+    commodities::Dict{Symbol, DataType}
+    time_data::Dict{Symbol, TimeData}
+    assets::Vector{AbstractAsset}
+    locations::Vector{Node}
+end
+
+function set_data_dirpath!(system::System, data_dirpath::String)
+    system.data_dirpath = data_dirpath
+end
+
+function add!(system::System, asset::AbstractAsset)
+    push!(system.assets, asset)
+end
+
+function add!(system::System, location::Node)
+    push!(system.locations, location)
+end
+
+function empty_system(data_dirpath::String)
+    return System(data_dirpath, NamedTuple(), Dict{Symbol, DataType}(), Dict{Symbol, TimeData}(), [], [])
+end
