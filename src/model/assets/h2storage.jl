1c71d0d4f8b99e8a1116b381101ce99c6bfae7b8
lbonaldo <bonaldo.luca12@gmail.com>
2024-08-29 19:57:37 -0400

    Small reformatting of h2storage.jl

--- src/model/assets/h2storage.jl
+++ src/model/assets/h2storage.jl
@@ -20,50 +20,50 @@ function make(::Type{H2Storage}, data::AbstractDict{Symbol,Any}, system::System)
 
     transform_data = process_data!(data[:transforms])
     compressor_transform = Transformation(;
-    id = Symbol(transform_data[:id]),
-    timedata = system.time_data[Symbol(transform_data[:time_commodity])],
-    constraints = get(transform_data, :constraints, [BalanceConstraint()])
+        id=Symbol(transform_data[:id]),
+        timedata=system.time_data[Symbol(transform_data[:time_commodity])],
+        constraints=get(transform_data, :constraints, [BalanceConstraint()])
     )
 
     compressor_elec_edge_data = process_data!(data[:edges][:compressor_elec])
     elec_start_node = find_node(system.locations, Symbol(compressor_elec_edge_data[:start_vertex]))
-    elec_end_node = compressor_transform;
-    compressor_elec_edge = Edge(Symbol(compressor_elec_edge_data[:id]),compressor_elec_edge_data, system.time_data[:Electricity],Electricity, elec_start_node,  elec_end_node)
-    compressor_elec_edge.unidirectional = get(compressor_elec_edge_data, :unidirectional, true);
+    elec_end_node = compressor_transform
+    compressor_elec_edge = Edge(Symbol(compressor_elec_edge_data[:id]), compressor_elec_edge_data, system.time_data[:Electricity], Electricity, elec_start_node, elec_end_node)
+    compressor_elec_edge.unidirectional = get(compressor_elec_edge_data, :unidirectional, true)
 
     compressor_h2_edge_data = process_data!(data[:edges][:compressor_h2])
     h2_start_node = find_node(system.locations, Symbol(compressor_h2_edge_data[:start_vertex]))
-    h2_end_node = compressor_transform;
-    compressor_h2_edge = Edge(Symbol(compressor_h2_edge_data[:id]),compressor_h2_edge_data, system.time_data[:Hydrogen],Hydrogen, h2_start_node,  h2_end_node)
-    compressor_h2_edge.unidirectional = get(compressor_h2_edge_data, :unidirectional, true);
+    h2_end_node = compressor_transform
+    compressor_h2_edge = Edge(Symbol(compressor_h2_edge_data[:id]), compressor_h2_edge_data, system.time_data[:Hydrogen], Hydrogen, h2_start_node, h2_end_node)
+    compressor_h2_edge.unidirectional = get(compressor_h2_edge_data, :unidirectional, true)
 
     charge_edge_data = process_data!(data[:edges][:charge])
-    charge_start_node =  compressor_transform;
-    charge_end_node =   h2storage;
-    h2storage_charge = Edge(Symbol(data[:id] * "_charge"),charge_edge_data, system.time_data[:Hydrogen],Hydrogen, charge_start_node,  charge_end_node)
-    h2storage_charge.unidirectional = get(charge_edge_data, :unidirectional, true);
-    h2storage_charge.constraints = get(charge_edge_data,:constraints,[CapacityConstraint()])
+    charge_start_node = compressor_transform
+    charge_end_node = h2storage
+    h2storage_charge = Edge(Symbol(data[:id] * "_charge"), charge_edge_data, system.time_data[:Hydrogen], Hydrogen, charge_start_node, charge_end_node)
+    h2storage_charge.unidirectional = get(charge_edge_data, :unidirectional, true)
+    h2storage_charge.constraints = get(charge_edge_data, :constraints, [CapacityConstraint()])
 
     discharge_edge_data = process_data!(data[:edges][:discharge])
     discharge_start_node = h2storage
     discharge_end_node = find_node(system.locations, Symbol(discharge_edge_data[:end_vertex]))
-    h2storage_discharge = Edge(Symbol(data[:id] * "_discharge"),discharge_edge_data, system.time_data[:Hydrogen],Hydrogen, discharge_start_node,  discharge_end_node);
-    h2storage_discharge.constraints = get(discharge_edge_data,:constraints,[CapacityConstraint(), RampingLimitConstraint()])
-    h2storage_discharge.unidirectional = get(discharge_edge_data, :unidirectional, true);
+    h2storage_discharge = Edge(Symbol(data[:id] * "_discharge"), discharge_edge_data, system.time_data[:Hydrogen], Hydrogen, discharge_start_node, discharge_end_node)
+    h2storage_discharge.constraints = get(discharge_edge_data, :constraints, [CapacityConstraint(), RampingLimitConstraint()])
+    h2storage_discharge.unidirectional = get(discharge_edge_data, :unidirectional, true)
 
     h2storage.discharge_edge = h2storage_discharge
     h2storage.charge_edge = h2storage_charge
 
-    h2storage.balance_data =  Dict(:storage=>Dict(h2storage_discharge.id=>1/get(discharge_edge_data,:efficiency,1.0),
-                                                h2storage_charge.id=>get(charge_edge_data,:efficiency,1.0)))
+    h2storage.balance_data = Dict(:storage => Dict(h2storage_discharge.id => 1 / get(discharge_edge_data, :efficiency, 1.0),
+        h2storage_charge.id => get(charge_edge_data, :efficiency, 1.0)))
 
-    compressor_transform.balance_data = Dict(:electricity=>Dict(compressor_h2_edge.id=>get(transform_data,:electricity_consumption,0.0),
-                                                compressor_elec_edge.id => 1.0,
-                                                h2storage_charge.id=>0.0),
-                                            :hydrogen=>Dict(h2storage_charge.id=>1.0,
-                                                            compressor_h2_edge.id => 1.0,
-                                                            compressor_elec_edge.id=>0.0),
-                                        )
+    compressor_transform.balance_data = Dict(:electricity => Dict(compressor_h2_edge.id => get(transform_data, :electricity_consumption, 0.0),
+            compressor_elec_edge.id => 1.0,
+            h2storage_charge.id => 0.0),
+        :hydrogen => Dict(h2storage_charge.id => 1.0,
+            compressor_h2_edge.id => 1.0,
+            compressor_elec_edge.id => 0.0),
+    )
 
     return H2Storage(id, h2storage, compressor_transform, h2storage_discharge, h2storage_charge, compressor_elec_edge, compressor_h2_edge)
 end
