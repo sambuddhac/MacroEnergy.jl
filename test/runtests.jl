f3b5d780fe019a04cd25d8f61049bc89a0b2e3f5
lbonaldo <bonaldo.luca12@gmail.com>
2024-09-10 19:47:46 -0400

    Add test to load functions and generate model

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,13 @@
 import Test
-using Logging, LoggingExtras
+using Logging
 using Macro
 
 
 test_logger = ConsoleLogger(stderr, Logging.Warn)
 
 with_logger(test_logger) do
-    Test.@testset "Configuration" begin
-        include("test_config.jl")
+    out = Test.@testset verbose = true "Load Inputs" begin
+        include("test_workflow.jl")
     end
-
-    Test.@testset verbose = true "Load Inputs" begin
-        include("test_load_inputs.jl")
-    end
-
+    return nothing
 end
---
+++ test/utilities.jl
@@ -0,0 +1,28 @@
+using Gurobi, Logging
+
+
+macro warn_error_logger(block)
+    quote
+        result = redirect_stdout(devnull) do
+            # Create a ConsoleLogger that prints any log messages with level >= Warn to stderr
+            warnerror_logger = ConsoleLogger(stderr, Logging.Warn)
+            with_logger(warnerror_logger) do
+                result = $(esc(block))
+            end
+        end
+        result
+    end
+end
+
+function is_gurobi_available()
+    try
+        @warn_error_logger Gurobi.Env();
+        return true
+    catch e
+        if isa(e, Gurobi.GurobiError)
+            return false
+        else
+            rethrow()
+        end
+    end
+end
