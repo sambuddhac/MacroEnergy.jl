9f981aea5b972cf1cbd2be1a5cc04de36a41f86d
Ruaridh Macdonald <rmacd@mit.edu>
2024-09-17 06:31:55 -0400

    Add functions to get struct fields and types

--- src/model/networks/asset.jl
+++ src/model/networks/asset.jl
@@ -1,1 +1,28 @@
 id(asset::AbstractAsset) = asset.id
+
+"""
+    struct_info(t::Type{T}) where T
+
+    Return a vector of tuples with the field names and types of a struct.
+"""
+function struct_info(::Type{T}) where T
+    names = fieldnames(T)
+    return [(names[idx], x) for (idx, x) in enumerate(T.types)]
+end
+
+function print_struct_info(asset::AbstractAsset)
+    info = struct_info(typeof(asset))
+    print_struct_info(info) 
+end
+
+function print_struct_info(asset::Type{<:AbstractAsset})
+    info = struct_info(asset)
+    print_struct_info(info) 
+end
+
+function print_struct_info(info::Vector{Tuple{Symbol, DataType}})
+    for (name, type) in info
+        println("Field: $name, Type: $type")
+    end    
+end
+
