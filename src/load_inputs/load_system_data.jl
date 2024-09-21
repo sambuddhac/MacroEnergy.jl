969bdd5bf34e590e87d9c49af39d903ee01009e7
Ruaridh Macdonald <rmacd@mit.edu>
2024-09-20 23:08:20 -0400

    Update load_system_data.jl
    - Fix bug in load_system_data(file_path) causing everything to load twice
    
    - Added doc strings to clarify the purpose of each method
    
    - Changed load_system_data!(system, file_path) to load_system_data(system, file_path) and have it just return the system_data, not load the data and then generate the model / alter the system. This is more similar to the other versions

--- src/load_inputs/load_system_data.jl
+++ src/load_inputs/load_system_data.jl
@@ -2,20 +2,12 @@
 # Internal functions to handle loading the system_data.json file
 ###### ###### ###### ###### ###### ######
 
-function load_system_data(
-    file_path::AbstractString;
-    default_file_path::String = joinpath(@__DIR__, "default_system_data.json"),
-    lazy_load::Bool = true,
-)::Dict{Symbol,Any}
-    load_system_data(
-        file_path,
-        dirname(file_path),
-        default_file_path = default_file_path,
-        lazy_load = lazy_load,
-    )
-    return load_json(file_path; lazy_load = lazy_load)
-end
+"""
+    load_system_data(file_path::AbstractString, rel_path::AbstractString; default_file_path::String = joinpath(@__DIR__, "default_system_data.json"), lazy_load::Bool = true)::Dict{Symbol,Any}
 
+Load the system data (currently only from a JSON file) given a file path and directory to search for the file in.
+All other file names defined in the system data file are assumed to be relative to rel_path.
+"""
 function load_system_data(
     file_path::AbstractString,
     rel_path::AbstractString;
@@ -30,27 +22,44 @@ function load_system_data(
     return load_json(file_path; lazy_load = lazy_load)
 end
 
-function load_system_data!(
-    system::System,
-    file_path::AbstractString,
+"""
+    load_system_data(file_path::AbstractString; default_file_path::String = joinpath(@__DIR__, "default_system_data.json"), lazy_load::Bool = true)::Dict{Symbol,Any}
+
+Load the system data (currently only from a JSON file) given a file_path.
+All other file names defined in the system data file are assumed to be relative to the parent directory of file_path.
+"""
+function load_system_data(
+    file_path::AbstractString;
     default_file_path::String = joinpath(@__DIR__, "default_system_data.json"),
-)::Nothing
-    # Load the provided system data
-    # We're assuming the file_path is a JSON file, not a directory
-    file_path = abspath(rel_or_abs_path(file_path, system.data_dirpath))
-
-    # Make sure the default arguments are included
-    prep_system_data(file_path, default_file_path)
-
-    # Load the system data from the JSON file(s)
-    system_data = load_json(file_path)
-
-    # Configure the model
-    load_system_data!(system, system_data)
-    return nothing
+    lazy_load::Bool = true,
+)::Dict{Symbol,Any}
+    return load_system_data(
+        file_path,
+        dirname(file_path),
+        default_file_path = default_file_path,
+        lazy_load = lazy_load,
+    )
 end
 
+"""
+    load_system_data(system::System, file_path::AbstractString)::Dict{Symbol,Any}
 
+Load the system data (currently only from a JSON file) given a file path and existing System.
+All other file names defined in the system data file are assumed to be relative to the data_dirpath field of the System.
+"""
+function load_system_data(
+    system::System, 
+    file_path::AbstractString
+)::Dict{Symbol,Any}
+    return load_system_data(file_path, system.data_dirpath)
+end
+
+"""
+    load_default_system_data(default_file_path::String)::Dict{Symbol,Any}
+
+Load the default system data from a JSON file. 
+This describes the default locations for the system data files.
+"""
 function load_default_system_data(
     default_file_path::String = joinpath(@__DIR__, "default_system_data.json"),
 )::Dict{Symbol,Any}
@@ -62,6 +71,11 @@ function load_default_system_data(
     return default_system_data
 end
 
+"""
+    add_default_system_data!(system_data::AbstractDict{Symbol,Any}, default_file_path::String)::Nothing
+
+Add the default system data to the system data dictionary. This adds any required fields that are missing.
+"""
 function add_default_system_data!(
     system_data::AbstractDict{Symbol,Any},
     default_file_path::String = joinpath(@__DIR__, "default_system_data.json"),
@@ -73,6 +87,12 @@ function add_default_system_data!(
     return nothing
 end
 
+"""
+    prep_system_data(file_path::AbstractString, default_file_path::String)::Nothing
+
+This attempts to load the system data from the file at file_path, adds any missing fields from the default system data, and writes the updated system data back to the file.
+In the future, we may change this to not write to the file, but for now, it's a quick way to ensure the system data is up-to-date.
+"""
 function prep_system_data(
     file_path::AbstractString,
     default_file_path::String = joinpath(@__DIR__, "default_system_data.json"),
@@ -87,7 +107,7 @@ function prep_system_data(
     # This could be moved to the settings defaults later
     add_default_system_data!(system_data, default_file_path)
 
-    # FIXME currenltly overwriting and then re-reading the system_data
+    # FIXME currently overwriting and then re-reading the system_data
     # This is a little janky, but lets us quickly use the JSON parsing functions
     open(file_path, "w") do io
         JSON3.pretty(io, system_data)
