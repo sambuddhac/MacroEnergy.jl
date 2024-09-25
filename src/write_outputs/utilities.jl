b8ae512fff3414a0663abae16b5ce761178f1e0e
lbonaldo <bonaldo.luca12@gmail.com>
2024-09-25 17:46:33 -0400

    Create OutputRow to represent a single result row

--- src/write_outputs/utilities.jl
+++ src/write_outputs/utilities.jl
@@ -1,3 +1,16 @@
+struct OutputRow
+    model::Union{Symbol,Missing}
+    scenario::Union{Symbol,Missing}
+    region::Symbol
+    variable::Symbol
+    unit::Symbol
+    time::Union{Int,Missing}
+    value::Float64
+    OutputRow(region::Symbol, variable::Symbol, unit::Symbol, value::Float64) =
+        new(missing, missing, region, variable, unit, missing, value)
+    OutputRow(region::Symbol, variable::Symbol, unit::Symbol, time::Int, value::Float64) =
+        new(missing, missing, region, variable, unit, time, value)
+end
 function write_csv(file_path::AbstractString, data::AbstractDataFrame)
     CSV.write(file_path, data)
 end
