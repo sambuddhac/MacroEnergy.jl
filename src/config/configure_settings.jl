b4ae161582a4fbfc70d457ebea4043b3e1560ff5
lbonaldo <bonaldo.luca12@gmail.com>
2025-01-13 09:58:56 +0100

    Remove deprecated implementation of `configure_time_interval!`

--- src/config/configure_settings.jl
+++ src/config/configure_settings.jl
@@ -52,31 +52,3 @@ function validate_names(settings::NamedTuple)
         error("Unknown settings: $(unknown_names)")
     end
 end
-
-function configure_time_interval!(
-    macro_settings::NamedTuple,
-    commodities::Dict{Symbol,DataType} = commodity_types(Macro),
-)
-    time_intervals = Dict{Any,StepRange{Int64,Int64}}()
-    subperiods = Dict{Any,Vector{StepRange{Int64,Int64}}}()
-    for (name, time_details) in macro_settings[:Commodities]
-        commodity_type = commodities[Symbol(name)]
-
-        period_length = macro_settings[:PeriodLength]
-        hours_per_timestep = time_details["HoursPerTimeStep"]
-        hours_per_subperiod = time_details["HoursPerSubperiod"]
-
-        time_interval = 1:hours_per_timestep:period_length
-        time_intervals[commodity_type] = time_interval
-
-        subperiods[commodity_type] = collect(
-            Iterators.partition(
-                time_interval,
-                Int(hours_per_subperiod / hours_per_timestep),
-            ),
-        )
-    end
-    macro_settings =
-        merge(macro_settings, [:TimeIntervals => time_intervals, :SubPeriods => subperiods])
-    return macro_settings
-end
