42ab132e552a737fe8d640d89a347c539626a6a6
lbonaldo <bonaldo.luca12@gmail.com>
2025-02-07 12:38:36 -0500

    Fix `get_unit` function to return different units depending on the attribute being passed

--- src/model/units.jl
+++ src/model/units.jl
@@ -2,28 +2,25 @@ const DEFAULT_UNITS = Dict{Symbol,Symbol}(
     :Electricity => :MWh,
     :Hydrogen => :MWh,
     :NaturalGas => :MWh,
+    :Uranium => :MWh,
     :CO2 => :t,
     :CO2Captured => :t,
     :Biomass => :t,
-    :Uranium => :t
 )
 
-"""
-    unit(commodity::Symbol)
+function convert_power_to_energy_unit(unit::Symbol)::Symbol
+    unit == :MWh && return :MW
+    return unit
+end
 
-Returns the default unit for a commodity `commodity` as a Julia `Symbol`.
-
-# Arguments
-- `commodity::Symbol`: The commodity to get the unit of. 
-
-# Returns
-- `Symbol`: The default unit corresponding to the input commodity. 
-
-# Example
-```julia
-unit(:Electricity)
-MWh
-```
-"""
-unit(commodity::Symbol) = DEFAULT_UNITS[commodity]
-unit(commodity::DataType) = unit(Symbol(commodity))
+function unit(commodity::Symbol, f::Function)::Symbol
+    unit = DEFAULT_UNITS[commodity]
+    if any(f .== [capacity, new_capacity, ret_capacity])
+        return convert_power_to_energy_unit(unit)
+    elseif any(f .== [flow, non_served_demand, storage_level])
+        return unit
+    else
+        @warn("Unit not supported.")
+    end
+end
+unit(commodity::DataType, f::Function)::Symbol = unit(Symbol(commodity), f)
