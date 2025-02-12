02bf06d8ee0010475ea776ffcb94c7375a2b9fa9
lbonaldo <bonaldo.luca12@gmail.com>
2025-02-12 17:31:09 -0500

    Update `write_capacity` to export data in wide format by default

--- src/write_outputs/capacity.jl
+++ src/write_outputs/capacity.jl
@@ -17,12 +17,13 @@ end
 
 # Wrapper functions
 """
-    get_optimal_capacity(system::System)
+    get_optimal_capacity(system::System; scaling::Float64=1.0)
 
 Get the optimal capacity values for all assets/edges in a system.
 
 # Arguments
 - `system::System`: The system containing the assets/edges to analyze
+- `scaling::Float64`: The scaling factor for the results.
 
 # Returns
 - `DataFrame`: A dataframe containing the optimal capacity values for all assets/edges, with missing columns removed
@@ -39,16 +40,16 @@ get_optimal_capacity(system)
    3 │ Electricity  capacity           elec_NE        existing_wind_NE                   existing_wind_NE_edge              VRE               capacity   3.6545
 ```
 """
-get_optimal_capacity(system::System) = get_optimal_capacity_by_field(system, capacity)
+get_optimal_capacity(system::System; scaling::Float64=1.0) = get_optimal_capacity_by_field(system, capacity, scaling)
 
 """
-    get_optimal_new_capacity(system::System)
+    get_optimal_new_capacity(system::System; scaling::Float64=1.0)
 
 Get the optimal new capacity values for all assets/edges in a system.
 
 # Arguments
 - `system::System`: The system containing the assets/edges to analyze
-
+- `scaling::Float64`: The scaling factor for the results.
 # Returns
 - `DataFrame`: A dataframe containing the optimal new capacity values for all assets/edges, with missing columns removed
 
@@ -64,16 +65,16 @@ get_optimal_new_capacity(system)
    3 │ Biomass      capacity           bioherb_NE     NE_BECCS_Electricity_Herb          NE_BECCS_Electricity_Herb_biomas…  BECCSElectricity  new_capacity      0.0
 ```
 """
-get_optimal_new_capacity(system::System) = get_optimal_capacity_by_field(system, new_capacity)
+get_optimal_new_capacity(system::System; scaling::Float64=1.0) = get_optimal_capacity_by_field(system, new_capacity, scaling)
 
 """
-    get_optimal_retired_capacity(system::System)
+    get_optimal_retired_capacity(system::System; scaling::Float64=1.0)
 
 Get the optimal retired capacity values for all assets/edges in a system.
 
 # Arguments
 - `system::System`: The system containing the assets/edges to analyze
-
+- `scaling::Float64`: The scaling factor for the results.
 # Returns
 - `DataFrame`: A dataframe containing the optimal retired capacity values for all assets/edges, with missing columns removed
 
@@ -89,14 +90,14 @@ get_optimal_retired_capacity(system)
    3 │ Biomass      capacity           bioherb_NE     NE_BECCS_Electricity_Herb          NE_BECCS_Electricity_Herb_biomas…  BECCSElectricity  retired_capacity  0.0
 ```
 """
-get_optimal_retired_capacity(system::System) = get_optimal_capacity_by_field(system, retired_capacity)
+get_optimal_retired_capacity(system::System; scaling::Float64=1.0) = get_optimal_capacity_by_field(system, retired_capacity, scaling)
 
 get_optimal_capacity(asset::AbstractAsset; scaling::Float64=1.0) = get_optimal_capacity_by_field(asset, capacity, scaling)
 get_optimal_new_capacity(asset::AbstractAsset; scaling::Float64=1.0) = get_optimal_capacity_by_field(asset, new_capacity, scaling)
 get_optimal_retired_capacity(asset::AbstractAsset; scaling::Float64=1.0) = get_optimal_capacity_by_field(asset, retired_capacity, scaling)
 
 """
-    write_capacity_results(file_path::AbstractString, system::System)
+    write_capacity(file_path::AbstractString, system::System; scaling::Float64=1.0, wide::Bool=true, drop_cols::Vector{Symbol}=Symbol[])
 
 Write the optimal capacity results for all assets/edges in a system to a file. 
 The extension of the file determines the format of the file.
@@ -105,22 +106,29 @@ The extension of the file determines the format of the file.
 # Arguments
 - `file_path::AbstractString`: The path to the file where the results will be written
 - `system::System`: The system containing the assets/edges to analyze
+- `scaling::Float64`: The scaling factor for the results
+- `wide::Bool`: Whether to write the results in wide format
+- `drop_cols::Vector{Symbol}`: Columns to drop from the DataFrame
 
 # Returns
 - `nothing`: The function returns nothing, but writes the results to the file
 
 # Example
 ```julia
-write_capacity_results(joinpath(results_dir, "all_capacity.csv"), system)
+write_capacity(joinpath(results_dir, "all_capacity.csv"), system)
 ```
 """
-function write_capacity_results(file_path::AbstractString, system::System)
+function write_capacity(file_path::AbstractString, system::System; scaling::Float64=1.0, wide::Bool=true, drop_cols::Vector{Symbol}=Symbol[])
     @info "Writing capacity results to $file_path"
-    capacity_results = get_optimal_capacity(system)
-    new_capacity_results = get_optimal_new_capacity(system)
-    retired_capacity_results = get_optimal_retired_capacity(system)
+    capacity_results = get_optimal_capacity(system; scaling)
+    new_capacity_results = get_optimal_new_capacity(system; scaling)
+    retired_capacity_results = get_optimal_retired_capacity(system; scaling)
     all_capacity_results = vcat(capacity_results, new_capacity_results, retired_capacity_results)
-    write_dataframe(file_path, all_capacity_results)
+    
+    # Reshape the dataframe based on the requested format
+    all_capacity_results = wide ? reshape_wide(all_capacity_results) : all_capacity_results
+    
+    write_dataframe(file_path, all_capacity_results, drop_cols)
     return nothing
 end
 
