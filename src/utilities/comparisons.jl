461452f5e06d76f59e28188e900d7217dd5b166c
Ruaridh Macdonald <rmacd@mit.edu>
2025-03-20 09:52:54 -0400

    Additional tools for comparing models, mostly assets

--- src/utilities/comparisons.jl
+++ src/utilities/comparisons.jl
@@ -14,50 +14,121 @@ function compare(a::Missing,b::Missing)
     return true
 end
 
-function Base.:(==)(a::MacroObject, b::MacroObject)
-    return all([compare(getproperty(a,prop),getproperty(b,prop)) for prop in propertynames(a)])
+function Base.:(==)(a::T, b::T) where {T<:Union{MacroObject,AbstractTimeData,AbstractTypeConstraint}}
+    return all([compare(getproperty(a,prop), getproperty(b,prop)) for prop in propertynames(a)])
 end
 
-function find_diff(a::MacroObject, b::MacroObject)
-    return [prop for prop in propertynames(a) if !compare(getproperty(a,prop),getproperty(b,prop))]
+function fun_check(f::Symbol, a, b, m::Module=MacroEnergy)
+    fun = getproperty(m, f)
+    try
+        return compare(fun(a), fun(b), false, m)
+    catch e
+        if isa(e, MethodError)
+            return compare(getproperty(a,f), getproperty(b,f), false, m)
+        else
+            return false
+        end
+    end
 end
 
-function Base.hash(a::MacroObject, h::UInt)
-    ht = hash(MacroObject, h)
+function fun_check(f::Symbol, a, b, idx, m::Module=MacroEnergy)
+    fun = getproperty(m, f)
+    try
+        return compare(fun(a, idx), fun(b, idx), false, m)
+    catch e
+        if isa(e, MethodError)
+            return compare(getproperty(a,f), getproperty(b,f), false, m)
+        else
+            return false
+        end
+    end
+end
+
+function compare(a, b, checkfunctions::Bool, m::Module=MacroEnergy)
+    return compare(a,b)
+end
+
+function compare(a::T, b::T, propname::Symbol, checkfunctions::Bool=false, m::Module=MacroEnergy) where {T<:Union{MacroObject,AbstractTimeData,AbstractTypeConstraint}}
+    @debug("Comparing property: $propname")
+    if MacroEnergy.compare(getproperty(a,propname), getproperty(b,propname), checkfunctions, m)
+        @debug("Data matched for $propname")
+        return true
+    end
+    if checkfunctions && isdefined(m, propname)
+        check1 = fun_check(propname, a, b)
+        check2 = fun_check(propname, a, b, 1)
+        @debug("Function comparison: $(check1 || check2) for $propname")
+        return check1 || check2
+    else
+        @debug("Data did not match for $propname")
+        return false
+    end
+end
+
+function compare(a::T, b::T, checkfunctions::Bool=false, m::Module=MacroEnergy) where {T<:Union{MacroObject,AbstractTimeData,AbstractTypeConstraint}}
+    comparisons = Bool[]
+    for propname in propertynames(a)
+        push!(comparisons, compare(a, b, propname, checkfunctions, m))
+    end
+    return all(comparisons)
+end
+
+function compare(a::AbstractStorage, b::AbstractStorage, checkfunctions::Bool=false, m::Module=MacroEnergy)
+    comparisons = Bool[]
+    for propname in propertynames(a)
+        @debug("Comparing property: $propname")
+        prop_a = getproperty(a, propname)
+        prop_b = getproperty(b, propname)
+        if isa(prop_a, AbstractEdge)
+            @debug("Comparing edge property: $propname")
+            test_result = compare(id(prop_a), id(prop_b), checkfunctions, m)
+            @debug("Test 1: $(test_result)")
+            push!(comparisons, test_result)
+            continue
+        end
+        push!(comparisons, compare(a, b, propname, checkfunctions, m))
+    end
+    return all(comparisons)
+end
+
+function find_diff(a::T, b::T, checkfunctions::Bool=false, m::Module=MacroEnergy) where {T<:Union{MacroObject,AbstractTimeData,AbstractTypeConstraint}}
+    return [prop for prop in propertynames(a) if !MacroEnergy.compare(getproperty(a,prop), getproperty(b,prop), checkfunctions, m)]
+end
+
+function Base.hash(a::T, h::UInt) where {T<:Union{MacroObject,AbstractTimeData,AbstractTypeConstraint}}
+    ht = hash(T, h)
     for prop in propertynames(a)
         ht = hash(getproperty(a,prop), ht)
     end
     return ht
 end
 
-function Base.:(==)(a::AbstractTimeData, b::AbstractTimeData)
-    return all([compare(getproperty(a,prop),getproperty(b,prop)) for prop in propertynames(a)])
-end
-
-function find_diff(a::AbstractTimeData, b::AbstractTimeData)
-    return [prop for prop in propertynames(a) if compare(getproperty(a,prop),getproperty(b,prop))]
-end
-
-function Base.hash(a::AbstractTimeData, h::UInt)
-    ht = hash(AbstractTimeData, h)
-    for prop in propertynames(a)
-        ht = hash(getproperty(a,prop), ht)
+function compare_assets(a::AbstractSystem, b::AbstractSystem, checkfunctions::Bool=false, m::Module=MacroEnergy; verbose::Bool=false)
+    if !verbose
+        return all(compare.(a.assets, b.assets, Ref(checkfunctions), Ref(m)))
+    else
+        comparisons = Bool[]
+        for (idx, asset_a) in enumerate(a.assets)
+            asset_b = get_asset_by_id(b, asset_a.id)
+            if isnothing(asset_b)
+                @debug("$(asset_a.id) not found in second system")
+                status = "⁉"
+                push!(comparisons, false)
+            else
+                # test_result = compare(asset_a, asset_b, checkfunctions, m)
+                # diff_string = ""
+                diff = find_diff(asset_a, asset_b, checkfunctions, m)
+                if isempty(diff)
+                    test_result = true
+                else
+                    test_result = false
+                end
+                status = test_result ? "🟩" : "🟥"
+                diff_string = isempty(diff) ? "" : "\n   -- Diff in: $(diff)"
+                push!(comparisons, test_result)
+            end
+            println("#$idx: $status | $(asset_a.id) | $(asset_b.id)" * diff_string)
+        end
+        return all(comparisons)
     end
-    return ht
-end
-
-function Base.:(==)(a::AbstractTypeConstraint, b::AbstractTypeConstraint)
-    return all([compare(getproperty(a,prop),getproperty(b,prop)) for prop in propertynames(a)])
-end
-
-function find_diff(a::AbstractTypeConstraint, b::AbstractTypeConstraint)
-    return [prop for prop in propertynames(a) if compare(getproperty(a,prop),getproperty(b,prop))]
-end
-
-function Base.hash(a::AbstractTypeConstraint, h::UInt)
-    ht = hash(AbstractTypeConstraint, h)
-    for prop in propertynames(a)
-        ht = hash(getproperty(a,prop), ht)
-    end
-    return ht
 end
