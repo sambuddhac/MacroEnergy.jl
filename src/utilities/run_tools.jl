2e1600c1d5f1f73fc4f2e2e757569074bdf08008
lbonaldo <bonaldo.luca12@gmail.com>
2025-03-26 00:15:13 -0400

    Simplify `run_case` to use stages

--- src/utilities/run_tools.jl
+++ src/utilities/run_tools.jl
@@ -1,48 +1,23 @@
-function run_case(case_path::AbstractString=@__DIR__; lazy_load::Bool=true, optimizer::DataType=HiGHS.Optimizer, optimizer_env::Any=missing)
+function run_case(
+    case_path::AbstractString=@__DIR__; 
+    lazy_load::Bool=true, 
+    optimizer::DataType=HiGHS.Optimizer, 
+    optimizer_env::Any=missing,
+    attributes::AbstractDict{Symbol,<:Any}=Dict(:BarConvTol=>1e-3, :Crossover => 0, :Method => 2)
+)
+
     println("###### ###### ######")
     println("Running case at $(case_path)")
 
-    system = load_system(case_path; lazy_load=lazy_load)
+    stages = load_stages(case_path; lazy_load=lazy_load)
 
-    model = generate_model(system)
+    optimizer = create_optimizer(optimizer, optimizer_env, attributes)
 
-    if !ismissing(optimizer_env)
-        try 
-            set_optimizer(model, () -> optimizer(optimizer_env));
-        catch
-            error("Error creating optimizer with environment. Check that the environment is valid.")
-        end
-    else
-        set_optimizer(model, optimizer);
-    end
-
-    try
-        set_optimizer_attributes(model, "BarConvTol"=>1e-8,"Crossover" => 1, "Method" => 2)
-    catch
-        @warn("Error setting optimizer attributes. Check that the optimizer is valid.")
-    end
-
-    if system.settings.ConstraintScaling
-        @info "Scaling constraints and RHS"
-        scale_constraints!(model)
-    end
-
-    optimize!(model)
+    (stages, model) = solve_stages(stages, optimizer)
     
-    ## Output results
-    # Create results directory
-    results_dir = create_output_path(system)
-    
-    # Capacity results
-    write_capacity(joinpath(results_dir, "capacity.csv"), system)
-    
-    # Cost results
-    write_costs(joinpath(results_dir, "costs.csv"), system, model)
+    write_outputs(case_path, stages, model)
 
-    # Flow results
-    write_flow(joinpath(results_dir, "flow.csv"), system)
-
-    return system, model
+    return stages.systems, model
 end
 
 function run_multistage_case(case_path::AbstractString=@__DIR__; num_stages::Int64=1, perfect_foresight::Bool = false, lazy_load::Bool=true, optimizer::DataType=HiGHS.Optimizer, optimizer_env::Any=missing)
