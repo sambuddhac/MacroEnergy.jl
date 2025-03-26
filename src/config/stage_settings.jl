5d4933d34f7f3e3396ebf8b717e517ca37f9593a
lbonaldo <bonaldo.luca12@gmail.com>
2025-03-26 00:08:39 -0400

    Add new nt for stage settings

---
+++ src/config/stage_settings.jl
@@ -0,0 +1,70 @@
+function default_stage_settings()
+    return Dict(
+        :StageLengths => [1],
+        :WACC => 0.,
+        :Myopic => true,
+        :PerfectForesight => false,
+    )
+end
+
+function configure_stages(
+    path::AbstractString,
+    rel_path::AbstractString,
+)
+    path = rel_or_abs_path(path, rel_path)
+    if isdir(path)
+        path = joinpath(path, "stage_settings.json")
+    end
+    if !isfile(path)
+        error("Settings file not found: $path")
+    end
+    return configure_stages(read_file(path))
+end
+
+function configure_stages(
+    stage_settings::AbstractDict{Symbol,Any},
+    rel_path::AbstractString,
+)
+    if haskey(stage_settings, :path)
+        @info("Configuring stages from path")
+        path = rel_or_abs_path(stage_settings[:path], rel_path)
+        return configure_stages(path, rel_path)
+    else
+        return configure_stages(stage_settings)
+    end
+end
+
+function configure_stages(stage_settings::AbstractDict{Symbol,Any})
+    @info("Configuring stages")
+    settings = default_stage_settings()
+    settings = merge(settings, stage_settings)
+    set_stage_lengths!(settings)
+    set_solution_algorithm!(settings)
+    validate_stage_settings(settings)
+    return namedtuple(settings)
+end
+
+function validate_stage_settings(stage_settings::AbstractDict{Symbol,Any})
+    @assert all(stage_settings[:StageLengths].>0)
+    @assert stage_settings[:WACC] >= 0
+end
+
+function set_stage_lengths!(stage_settings::AbstractDict{Symbol,Any})
+    stage_settings[:StageLengths] = copy(stage_settings[:StageLengths])
+    return nothing
+end
+
+function set_solution_algorithm!(stage_settings::AbstractDict{Symbol,Any})
+    @info("Setting solution algorithm")
+    if stage_settings[:Myopic]
+        stage_settings[:SolutionAlgorithm] = Myopic()
+    elseif stage_settings[:PerfectForesight]
+        stage_settings[:SolutionAlgorithm] = PerfectForesight()
+    else
+        error("Invalid solution algorithm: $(stage_settings[:SolutionAlgorithm])")
+    end
+    delete!(stage_settings, :Myopic)
+    delete!(stage_settings, :PerfectForesight)
+    @info("Solution algorithm set to $(stage_settings[:SolutionAlgorithm])")
+    return nothing
+end
