7a0f3af13ff580c68e9ed979468f690b61122d26
lbonaldo <bonaldo.luca12@gmail.com>
2025-04-03 21:50:54 -0400

    Update `run_benders.jl`

--- ExampleSystems/eastern_us_three_zones/run_benders.jl
+++ ExampleSystems/eastern_us_three_zones/run_benders.jl
@@ -3,21 +3,22 @@
 ### using Pkg; Pkg.develop(path="path-to-MacroEnergySolvers.jl")
 
 using MacroEnergy
-using MacroEnergySolvers
 using Gurobi
+
 
 case_path = @__DIR__;
 
-mono_stages = MacroEnergy.load_stages(case_path; lazy_load=true)
+mono_stages = MacroEnergy.load_stages(case_path; lazy_load=true);
 
 if !(@isdefined GRB_ENV)
     const GRB_ENV = Gurobi.Env()
 end
 
-mono_optimizer = MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6))
+benders_optimizer = Dict(
+    :planning => MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6)),
+    :subproblems => MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6))
+)
 
-(mono_stages, mono_model) = MacroEnergy.solve_stages(mono_stages, mono_optimizer)
+(mono_stages, mono_model) = MacroEnergy.solve_stages(mono_stages, benders_optimizer);
 
-stages = MacroEnergy.load_stages(case_path; lazy_load=true);
-
-println("")
+println("Done!")
