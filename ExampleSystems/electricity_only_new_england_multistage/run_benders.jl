98792ca017ba400b14a7063d652afa95faaa11ee
Filippo Pecci <peccifilippo@gmail.com>
2025-04-09 14:36:18 +0200

    Tests with serial benders decomposition for multistage - looks OK

--- ExampleSystems/electricity_only_new_england_multistage/run_benders.jl
+++ ExampleSystems/electricity_only_new_england_multistage/run_benders.jl
@@ -12,13 +12,21 @@ if !(@isdefined GRB_ENV)
     const GRB_ENV = Gurobi.Env()
 end 
 
-stages = MacroEnergy.load_stages(case_path; lazy_load=true);
-
 benders_optimizers = Dict(
-    :planning => MacroEnergy.create_optimizer(Gurobi.Optimizer,GRB_ENV,("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3)),
-    :subproblems => MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-3)),
+    :planning => MacroEnergy.create_optimizer(Gurobi.Optimizer,GRB_ENV,("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6)),
+    :subproblems => MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-6)),
 )
+mono_optimizer = MacroEnergy.create_optimizer(Gurobi.Optimizer, GRB_ENV, ("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6))
 
-stages, results = MacroEnergy.solve_stages(stages, benders_optimizers);
+stages = MacroEnergy.load_stages(case_path; lazy_load=true);
+stages,bd_results = MacroEnergy.solve_stages(stages, benders_optimizers,MacroEnergy.PerfectForesight(),MacroEnergy.Benders());
+
+mono_stages = MacroEnergy.load_stages(case_path; lazy_load=true);
+mono_stages,mono_model = MacroEnergy.solve_stages(mono_stages,mono_optimizer,MacroEnergy.PerfectForesight(),MacroEnergy.Monolithic());
+
+_bd_capacity = MacroEnergy.get_available_capacity(stages.systems);
+bd_capacity = Dict(y=>MacroEnergy.value(_bd_capacity[y]) for y in keys(_bd_capacity));
+_mono_capacity = MacroEnergy.get_available_capacity(mono_stages.systems);
+mono_capacity = Dict(y=>MacroEnergy.value(_mono_capacity[y]) for y in keys(_mono_capacity));
 
 println("Done!")
