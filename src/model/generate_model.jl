9e852f2beb0587cc1e304eef24b128a030f7a347
Filippo Pecci <peccifilippo@gmail.com>
2025-05-06 10:10:55 +0200

    Move existing capacity validation to generate_model.jl

--- src/model/generate_model.jl
+++ src/model/generate_model.jl
@@ -341,3 +341,15 @@ end
 function undo_discount_fixed_costs!(n::Node,settings::NamedTuple)
     return nothing
 end
+
+function validate_existing_capacity(asset::AbstractAsset)
+    for t in fieldnames(typeof(asset))
+        if isa(getfield(asset, t), AbstractEdge) || isa(getfield(asset, t), AbstractStorage)
+            if existing_capacity(getfield(asset, t)) > 0
+                msg = " -- Asset with id: \"$(id(asset))\" has existing capacity equal to $(existing_capacity(getfield(asset,t)))"
+                msg *= "\nbut it was not present in the previous stage. Please double check that the input data is correct."
+                @warn(msg)
+            end
+        end
+    end
+end
--- src/model/multistage.jl
+++ src/model/multistage.jl
@@ -1,13 +1,2 @@
 
 
-function validate_existing_capacity(asset::AbstractAsset)
-    for t in fieldnames(typeof(asset))
-        if isa(getfield(asset, t), AbstractEdge) || isa(getfield(asset, t), AbstractStorage)
-            if existing_capacity(getfield(asset, t)) > 0
-                msg = " -- Asset with id: \"$(id(asset))\" has existing capacity equal to $(existing_capacity(getfield(asset,t)))"
-                msg *= "\nbut it was not present in the previous stage. Please double check that the input data is correct."
-                @warn(msg)
-            end
-        end
-    end
-end
