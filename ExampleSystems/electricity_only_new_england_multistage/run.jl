9f2357f8d6d8407f06fc6c8325fc0cb2c0dc6e23
Filippo Pecci <peccifilippo@gmail.com>
2025-05-29 21:43:12 +0200

    update test system

--- ExampleSystems/electricity_only_new_england_multistage/run.jl
+++ ExampleSystems/electricity_only_new_england_multistage/run.jl
@@ -3,4 +3,4 @@ using Gurobi
 
 (system, model) = run_case(@__DIR__; 
                     optimizer=Gurobi.Optimizer,
-                    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-6));
+                    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3));
