a7fb4745a866e16aed4d00e03c5588e78c33c989
Anna Li <li.anna6389@gmail.com>
2025-05-30 09:51:27 -0400

    updates

--- ExampleSystems/eastern_us_three_zones_reduced_cement/run.jl
+++ ExampleSystems/eastern_us_three_zones_reduced_cement/run.jl
@@ -23,3 +23,8 @@ MacroEnergy.set_optimizer(model, Gurobi.Optimizer);
 MacroEnergy.set_optimizer_attributes(model, "BarConvTol"=>1e-3,"Crossover" => 0, "Method" => 2)
 
 MacroEnergy.optimize!(model)
+
+for i in 1:length(system.assets)
+    asset = system.assets[i]
+    println(string(i) * string(asset.id))
+end
