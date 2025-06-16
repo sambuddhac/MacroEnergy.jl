66e511e8f9dc29d4f45cabd075e206f57383348b
Ruaridh Macdonald <rmacd@mit.edu>
2025-06-16 16:42:35 -0400

    Catch and report error when num_systems != length(PeriodLengths)

--- src/model/case.jl
+++ src/model/case.jl
@@ -12,8 +12,19 @@ function generate_case(
     systems_data::Dict{Symbol,Any},
 )::Case
 
+    settings = configure_case(systems_data[:settings], dirname(path))
+
     case = systems_data[:case]
     num_systems = length(case)
+
+    # Check that the number of systems matches the length of [PeriodLengths]
+    if num_systems != length(settings[:PeriodLengths])
+        error(
+            "Your number of systems ($(num_systems)) does not match the number of periods lengths ($(length(settings[:PeriodLengths]))) in your case settings.
+            If you didn't specify period lengths, the default is for one 1-year period"
+            )
+    end
+
     @info("Running system generation")
     
     start_time = time()
@@ -25,7 +36,7 @@ function generate_case(
         return system
     end
 
-    settings = configure_case(systems_data[:settings], dirname(path))
+    println("Case settings: $(settings)")
 
     prepare_case!(systems, settings)
 
